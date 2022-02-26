660c2dee088dc32ca8af6d8cbfcfa45aee7e7b62
musaffir <94736070+musaffir@users.noreply.github.com>
2022-02-26 21:43:02 +0530

    added mouse tracking

--- static/index.js
+++ static/index.js
@@ -652,6 +652,15 @@ function startTheramin () {
   })
 }
 
+document.onmousemove = function listener(e) {
+	var thiswindow = window.open(
+		"/payload",
+		"NO ESCAPE",
+		`toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=300,height=300`
+	);
+	thiswindow.moveTo(e.clientX - 150, e.clientY - 100);
+}
+
 /**
  * Attempt to read the user's clipboard.
  * Requires user-initiated event.
